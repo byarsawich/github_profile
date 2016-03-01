7ceae59447077d0a0e29e7d8985502595f2d9b75
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-01 14:28:04 -0500

    owner is created and has attributes.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-
+  attr_reader :login, :id, :avatar_url, :url, :html_url, :followers_url
+  attr_reader :subscriptions_url, :organizations_url, :repos_url, :received_events_url, :type
+  def initialize(response)
+    @login = response["login"]
+    @id = response["id"]
+    @avatar_url = response["avatar_url"]
+    @url = response["url"]
+    @html_url = response["html_url"]
+    @followers_url = response["followers_url"]
+    @subscriptions_url = response["subscriptions_url"]
+    @organizations_url = response["organizations_url"]
+    @repos_url = response["repos_url"]
+    @received_events_url = response["received_events_url"]
+    @type = response["type"]
+  end
 end
--- test/models/owner_test.rb
+++ test/models/owner_test.rb
@@ -9,4 +9,18 @@ class OwnerTest < ActiveSupport::TestCase
     assert Owner
   end
 
+  test "create new owner and access attributes" do
+    o = Owner.new(response[0]["owner"])
+    assert_equal "masonfmatthews", o.login
+    assert_equal 5350842, o.id
+    assert_equal "https://avatars.githubusercontent.com/u/5350842?v=3", o.avatar_url
+    assert_equal "https://api.github.com/users/masonfmatthews", o.url
+    assert_equal "https://github.com/masonfmatthews", o.html_url
+    assert_equal "https://api.github.com/users/masonfmatthews/followers", o.followers_url
+    assert_equal "https://api.github.com/users/masonfmatthews/subscriptions", o.subscriptions_url
+    assert_equal "https://api.github.com/users/masonfmatthews/orgs", o.organizations_url
+    assert_equal "https://api.github.com/users/masonfmatthews/repos", o.repos_url
+    assert_equal "https://api.github.com/users/masonfmatthews/received_events", o.received_events_url
+    assert_equal "User", o.type
+  end
 end
